3d14286ea98de3a06716799c65de1cf027bbd018
Harlan Wilton <harlan@harlanzw.com>
2026-03-26 04:38:01 +1100

    chore: sync

--- src/kit.ts
+++ src/kit.ts
@@ -1,7 +1,2 @@
-export { hookNuxtSeoProLicense, registerNuxtSeoProModule } from 'nuxtseo-shared/pro'
-export type { ModuleRegistration } from 'nuxtseo-shared/pro'
-
-/**
- * @deprecated Use registerNuxtSeoProModule instead
- */
-export { registerNuxtSeoProModule as registerModule } from 'nuxtseo-shared/pro'
+export type { NuxtSeoModuleDetection } from 'nuxtseo-shared/kit'
+export { hookNuxtSeoProLicense } from 'nuxtseo-shared/pro'
